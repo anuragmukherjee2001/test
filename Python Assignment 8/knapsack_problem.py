cfe905024d448abae6384ad8ab71cfd86fca488c
Anurag <anuragmukherjee2001@gmail.com>
2020-12-20 20:15:52 +0530

    Added on 20/12/2020

--- Python Assignment 8/knapsack_problem.py
+++ Python Assignment 8/knapsack_problem.py
@@ -3,20 +3,49 @@
 #  p= [280,100,120,120]
 #  profit = 440
 #  capacity = 60
-max_weight = int(input("Enter the maximum weight: "))
 
-dict1 = {}
-num1 = int(input("Enter the numbers of elements for the first dictionary: "))
-key1 = ''
 
-print("Enter theh value for the first dictionary")
+def knapsack(profit, weight, capacity):
 
-for i in range(num1):
-    key1 = int(input("Enter the weight "))
-    value1 = int(input("Enter the Profit"))
-    dict1[key1] = value1
+    index = list(range(len(profit)))
+    # print(index)
 
-weight_list = [dict1.keys()]
-print(weight_list[1])
+    ratio = [p/w for p, w in zip(profit, weight)]
+    # print(ratio)
 
-# This program is not completed
+    index.sort(key=lambda i: ratio[i], reverse=True)
+    # print(index)
+ 
+    max_profit = 0
+    
+    for i in index:
+        if weight[i] <= capacity:
+            
+            max_profit += profit[i]
+            capacity -= weight[i]
+        else:            
+            max_profit += profit[i]*capacity/weight[i]
+            break
+ 
+    return max_profit
+ 
+ 
+n = int(input('Enter number of items: '))
+print("Enter the profit values: ")
+profit = []
+
+for i in range(n):
+    inp = int(input())
+    profit.append(inp)
+    
+print("Enter the weight values: ")
+weight = []
+
+for i in range(n):
+    inp = int(input())
+    weight.append(inp)
+
+capacity = int(input('Enter maximum Capacity: '))
+ 
+max_profit = knapsack(profit, weight, capacity)
+print('The maximum profit of items that can be carried:', max_profit)
